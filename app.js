a4f972b2905ab2c9af879822b0631b0139c9539a
Matthew <matthew.elswick@matthe815.dev>
2021-10-06 16:14:24 -0400

    feature: Regular system checks for tracker-plugin connections　(#30)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @type{import('./public/networking/auto_tracker')}
  */
 const AutoTracker = new (require('./js/networking/auto_tracker'))()
-AutoTracker.Initalize() // Initalize the emulator-linked auto tracker.
+//AutoTracker.Initalize() // Initalize the emulator-linked auto tracker.
 
 const WebServer = new (require('./js/renderer/webserver'))()
 
@@ -16,6 +16,16 @@ Electron.on('data', (data) => {
   AutoTracker.Send(data)
 })
 
+AutoTracker.on('tracker status', status => {
+  console.log('Tracker connection: ' + status)
+  Electron.SendData(JSON.stringify({ payload: 4, data: status }))
+})
+
 AutoTracker.on('data', (data) => {
-  Electron.SendData(data)
+  console.log(data)
+  try {
+    Electron.SendData(data)
+  } catch (e) {
+    console.log(e)
+  }
 })

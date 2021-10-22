9bc446cfbace70581d5e1c5aab7acee786660b45
Matthew <matthew.elswick@matthe815.dev>
2021-10-22 02:38:28 -0400

    language support integrated

--- public/js/components/Player.js
+++ public/js/components/Player.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import app from '../app'
 import Parser from '../classes/Parser'
+import Translator from '../classes/Translator'
+import LanguageContext from '../components/LanguageContext'
 
 export default class Player extends React.Component {
+  static contextType = LanguageContext
   constructor (props) {
     super(props)
     this.state = { world: this.props.world, save: this.props.save }
@@ -39,7 +42,7 @@ export default class Player extends React.Component {
         <div style={this.props.current ? {color: '#222'} : null} className='character_name' onClick={() => (app.local.world = app.worlds[this.props.world])}><span>{this.state.save.player_name}</span> <span style={this.state.save.rupee_count == app.worlds[this.state.world].items.wallet.value ? {color: '#e08231'} : null }><img width='16' src={['/images/green_rupee.png', '/images/blue_rupee.png', '/images/red_rupee.png'][app.worlds[this.state.world].items.wallet.Index()]} />{this.state.save.rupee_count}</span></div>
         <div className='heart_containers'>{this.generateContainers()}</div>
         <div className='progress' style={{width: app.worlds[this.state.world].save.magic_meter_size+'%'}}><div className='progress-bar' style={{width: app.worlds[this.state.world].save.magic_current+'%'}} /></div>
-        {app.worlds.length > 1 ? <div>World {this.state.world+1}</div> : null } {Parser.ParseScenes()[app.worlds[this.state.world].scene].name}
+        {app.worlds.length > 1 ? <div>{Translator.GetTranslation(this.context.language, "World")} {this.state.world+1}</div> : null } {Parser.ParseScenes()[app.worlds[this.state.world].scene].name}
       </div>
     )
   }
--- public/js/components/Sidebar.js
+++ public/js/components/Sidebar.js
@@ -6,13 +6,16 @@ import Items from './Items'
 import Dungeons from './Dungeon'
 import Settings from './Settings'
 import Worlds from './Worlds'
+import LanguageContext from './LanguageContext'
+import Translator from '../classes/Translator'
 
 export class SidebarButtons extends React.Component {
+  static contextType = LanguageContext
+
   constructor (props) {
     super(props)
     this.state = { uploaded: false, worldId: app.global.world }
     this.handleChange = this.handleChange.bind(this)
-
   }
 
   handleChange (e) {
@@ -22,10 +25,10 @@ export class SidebarButtons extends React.Component {
   render () {
     return (
       <select className='form-control' onChange={this.handleChange}>
-        <option value='0'>Worlds</option>
-        <option value='1'>Saves</option>
-        <option value='3'>Items</option>
-        <option value='4'>Settings</option>
+        <option value='0'>{Translator.GetTranslation(this.context.language, 'World')}</option>
+        <option value='1'>{Translator.GetTranslation(this.context.language, 'Saves')}</option>
+        <option value='3'>{Translator.GetTranslation(this.context.language, 'Items')}</option>
+        <option value='4'>{Translator.GetTranslation(this.context.language, 'Settings')}</option>
       </select>
     )
   }
@@ -52,12 +55,17 @@ class PlayerList extends React.Component {
 }
 
 export default class Sidebar extends React.Component {
+  static contextType = LanguageContext
   constructor (props) {
     super(props)
     this.state = { page: 0 }
 
     this.handleChange = this.handleChange.bind(this)
 
+    this.pages = [this.homePage()];
+  }
+
+  componentDidMount () {
     this.pages = [this.homePage(), this.savePage(), this.worldPage(), this.itemPage(), this.settingsPage()]
   }
 
@@ -100,6 +108,7 @@ export default class Sidebar extends React.Component {
     return (
       <>
         <input type='file' id='spoiler' onChange={(e) => this.props.onSpoilerUpload(e)} accept='.json' style={{display: 'none'}} />
+        <select className='btn btn-default form-control' style={{width: '100%'}} onChange={(e) => this.context.languageChange(e)}>{Translator.GetLanguages().map((lang) => <option value={lang}>{lang}</option>)}</select>
         <button className='btn btn-default form-control' style={{width: '100%', borderBottom: '1px solid #555'}} onClick={(e) => $('#spoiler').click()}>Upload Spoiler Log</button>
         <Settings settings={app.global.settings} />
         <hr />

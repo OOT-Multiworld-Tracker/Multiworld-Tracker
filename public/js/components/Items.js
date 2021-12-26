13f61e79feb5838746f7972a08f73af2c1839212
Matthew <matthew.elswick@matthe815.dev>
2021-12-25 20:57:51 -0500

    refactor: Additional item-list opt and unused code removal

--- public/js/components/Items.js
+++ public/js/components/Items.js
@@ -9,7 +9,7 @@ export default class Items extends React.Component {
   static contextType = LanguageContext
   constructor () {
     super()
-    this.state = { items: app.local.world.save, search: "" }
+    this.state = { items: app.local.world.save }
 
     this.onWorldUpdate = this.onWorldUpdate.bind(this)
   }
@@ -26,42 +26,50 @@ export default class Items extends React.Component {
     app.unsubscribe('world update', this.onWorldUpdate)
   }
 
+  makeGridElement (item) {
+    return (
+      <div 
+        key={item.name+index} 
+        className={`grid-item${item.Index() > 0 ? " active" : ""}`} 
+        onClick={() => {item.Toggle(); this.setState({ items: app.local.world.save })}}>
+
+          <div style={{backgroundImage: `url(${item.Icon()})`}}>
+            { item.values.length > 2 && item.Index ( ) > 0 && 
+              <span>
+                { (typeof item.value != "number") ? item.value.slice ( 0, 1 ) : item.value }
+              </span>
+            }
+          </div>
+
+      </div>
+    )
+  }
+
   render () {
     return (
       <div style={{maxWidth: '260px',flexWrap: 'wrap', display:'flex'}}>
-        {Object.values(app.local.world.items).filter((item) => item.name == this.state.search || this.state.search == '').map(
-          (item, index) => {
-            if (item instanceof KeyManager) {
-              const keyList = [
-                <div className='list-header' key={item.name}>
-                  <span className='location-name'>{GetTranslation(this.context.language, item.name)}</span>
-                  <span className='location-items'>Have</span>
-                </div>
-              ];
+      {
+        // Turn to item list into an array of values for indexing.
+        Object.values(app.local.world.items).map( item => {
+          if (item instanceof KeyManager) {
+            const keyList = [
+              <div className='list-header' key={item.name}>
+                <span className='location-name'>{GetTranslation(this.context.language, item.name)}</span>
+                <span className='location-items'>Have</span>
+              </div>
+            ];
 
-              Object.values(item).forEach(keyManager => {
-                if (!(keyManager instanceof Item)) return;
-
-                keyList.push(
-                  <div className={`grid-item${keyManager.Index() > 0 ? " active" : ""}`} key={keyManager.name} onClick={() => { keyManager.Toggle(); this.setState({ items: app.local.world.save }) }}>
-                    <div style={{backgroundImage: `url(${keyManager.Icon()})`}}>
-                      {keyManager.values.length > 2 && keyManager.Index() > 0 && <span>{keyManager.value}/{keyManager.values.length-1}</span>}
-                    </div>
-                  </div>
-                );
-              });
+            Object.values(item).forEach(keyManager => {
+              if (!(keyManager instanceof Item)) return;
+              keyList.push(this.makeGridElement(keyManager)); 
+            });
             
             return keyList;
           }
 
-          return (
-            <div key={item.name+index} className={`grid-item${item.Index() > 0 ? " active" : ""}`} onClick={() => {item.Toggle(); this.setState({ items: app.local.world.save })}}>
-              <div style={{backgroundImage: `url(${item.Icon()})`}}>
-                {item.values.length > 2 && item.Index() > 0 && <span>{typeof item.value != "number" ? item.value.slice(0,1) : item.value}</span>}
-              </div>
-            </div>
-          )
-        })}
+          return this.makeGridElement(item);
+        })
+      }
       </div>
     )
   }
